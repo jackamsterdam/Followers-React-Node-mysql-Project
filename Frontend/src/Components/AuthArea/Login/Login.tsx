c85fda75e7c7603d219d396df93bd644c58e1969
Jack Amsterdam <64046793+jackamsterdam@users.noreply.github.com>
2022-03-28 02:11:11 +0300

    check if admin and navigate to admins portal that doesnt exist yet

--- Frontend/src/Components/AuthArea/Login/Login.tsx
+++ Frontend/src/Components/AuthArea/Login/Login.tsx
@@ -5,6 +5,7 @@ import "./Login.css";
 import { useForm } from 'react-hook-form'
 import notify from "../../../Services/NotifyService";
 import authService from "../../../Services/AuthService";
+import store from "../../../Redux/Store";
 // import { RefObject, useRef } from "react";
 
 function Login(): JSX.Element {
@@ -22,7 +23,17 @@ async function submit(credentials: CredentialsModel): Promise<void> {
         console.log("credentials", credentials);
         try {
             await authService.login(credentials)
+
+            //Check if user role in token is admin and if so navigate to admins portal
+            if(store.getState().authState.user.roleId === 2) {
+                navigate('/admin/home')
+                return
+            }
+            console.log("store.getState().authState.user.roleId", store.getState().authState.user.roleId);
+
             notify.success('You are now logged-in!')
+            
+            //if user.roleId = 1 -regular user than navigate to home
             navigate('/home')
             
             
